5ff63e7cfb3f85fb791bb9ac8e64ce4e01f05f8d
Arthur Oliveira Silva <arthurolsi2003@gmail.com>
2023-07-04 13:13:00 -0300

    Add files via upload

---
+++ Ex. 05 - 2019.sql
@@ -0,0 +1,22 @@
+/* Atividade realizada em 2019 */
+
+CREATE DEFINER=`root`@`localhost` TRIGGER `cantina`.`testauto` BEFORE INSERT
+    ON `cantina`.`pedido`
+    FOR EACH ROW 
+    BEGIN
+         SET NEW.valor_final  = (select qtd from salgados
+								where salgados.id_salgados = NEW.id_salgados) 
+                                                                                 
+													* 
+                                                                                 
+								(select precosalgado from salgados
+								where salgados.id_salgados = NEW.id_salgados);
+                                
+                                SET NEW.valor_final  = (select qtd from bebidas
+								where bebidas.id_bebidas = NEW.id_bebidas) 
+                                                                                 
+													* 
+                                                                                 
+								(select precofudido from bebidas
+								where bebidas.id_bebidas = NEW.id_bebidas);
+    END
